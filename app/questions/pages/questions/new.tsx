2170e211429ee1df9ec27783b5c0b9b723f79dc5
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2021-01-30 21:26:43 +0900

    [update] Writing a minimal form

--- app/questions/pages/questions/new.tsx
+++ app/questions/pages/questions/new.tsx
@@ -13,13 +13,22 @@ const NewQuestionPage: BlitzPage = () => {
 
       <QuestionForm
         initialValues={{}}
-        onSubmit={async () => {
+        onSubmit={async (event) => {
           try {
             const question = await createQuestionMutation({
-              data: { text: "Do you love Blitz?", choices: { create: [{ text: "Yes!" }] } },
+              data: {
+                text: event.target[0].value,
+                choices: {
+                  create: [
+                    { text: event.target[1].value },
+                    { text: event.target[2].value },
+                    { text: event.target[3].value },
+                  ],
+                },
+              },
             })
             alert("Success!" + JSON.stringify(question))
-            router.push(`/questions/${question.id}`)
+            router.push("/questions/[questionId]", `/questions/${question.id}`)
           } catch (error) {
             alert("Error creating question " + JSON.stringify(error, null, 2))
           }
